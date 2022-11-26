1ec5471123d7deaf5df4f2a137e3bd48d36b7fbf
“Yz_Y” <2592699730@qq.com>
2022-11-26 16:57:47 +0800

    demo04

---
+++ demo04/test/src/App.vue
@@ -0,0 +1,346 @@
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">你需要做点什么</h2>
+      <input type="text" placeholder="请输入名字" v-model="name">
+    </section>
+
+    <section class="create-todo">
+      <h3>创建一个待办事项！</h3>
+      <form @submit.prevent="addTodo">
+        <h4>在这里输入你的待办事项</h4>
+        <input type="text" placeholder="示例：制作一条短视频！" v-model="input_content">
+
+        <h4>选择一个事项类习惯</h4>
+        <div class="options">
+          <label>
+            <input type="radio" name="category" value="bussiness" v-model="input_category">
+            <span class="bubble bussiness"></span>
+            <div>业务</div>
+          </label>
+          <label>
+            <input type="radio" name="category" value="bussiness" v-model="input_category">
+            <span class="bubble personal"></span>
+            <div>业务</div>
+          </label>
+        </div>
+        <input type="submit" value="添加事项">
+      </form>
+    </section>
+    <section class="todo-list">
+      <h3>待办事项</h3>
+      <div class="list">
+        <div v-for="todo in todos" :key="todo.index" :class="`todo-item ${todo.done && 'done'}`">
+          <label>
+            <input type="checkbox" v-model="todo.done">
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+          <div class="todo-content">
+            <input type="text" v-model="todo.content">
+          </div>
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">删除</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+      name: '',
+      input_content: '',
+      input_category: null,
+    };
+  },
+  mounted() {
+    this.name = localStorage.getItem('name' || '')
+    this.todos = JSON.parse(localStorage.getItem('todos') || [])
+  },
+  computed: {
+    todos_asc: function () {
+      this.todos.sort((a, b) => {
+        return b.createAt - a.createAt
+      })
+
+    }
+  },
+  watch: {
+    todos: {
+      deep: true,
+      handler(newVal) {
+        localStorage.setItem('todos', JSON.stringify(newVal))
+      },
+    },
+    name: {
+      deep: true,
+      handler(newVal) {
+        localStorage.setItem('name', newVal)
+      },
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.input_content.trim() === '' || this.input_category.value === null) {
+        return
+      } else {
+        this.todos.push({
+          content: this.input_content,
+          category: this.input_category,
+          done: false,
+          createAt: new Date().getTime()
+        })
+        this.input_content = '';
+        this.input_content = null;
+      }
+    },
+    removeTodo(todo) {
+      this.todos = this.todos.filter(t => t !== todo)
+    }
+  }
+}
+// const todos = ref([])
+// const name = ref('')
+// const input_content = ref('')
+// const input_category = ref(null)
+
+</script>
+
+<style>
+:root {
+  --primary: #EA40A4;
+  --business: #3A82EE;
+  --personal: var(--primary);
+  --light: #EEE;
+  --grey: #888;
+  --dark: #313154;
+  --danger: #ff5b57;
+
+  --shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+
+  --business-glow: 0px 0px 4px rgba(58, 130, 238, 0.75);
+  --personal-glow: 0px 0px 4px rgba(234, 64, 164, 0.75);
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'montserrat', sans-serif;
+}
+
+input:not([type="radio"]):not([type="checkbox"]),
+button {
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  cursor: initial;
+}
+
+body {
+  background: var(--light);
+  color: var(--dark);
+}
+
+section {
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  padding-left: 1.5rem;
+  padding-right: 1.5em;
+}
+
+h3 {
+  color: var(--dark);
+  font-size: 1rem;
+  font-weight: 400;
+  margin-bottom: 0.5rem;
+}
+
+h4 {
+  color: var(--grey);
+  font-size: 0.875rem;
+  font-weight: 700;
+  margin-bottom: 0.5rem;
+}
+
+.greeting .title {
+  display: flex;
+}
+
+.greeting .title input {
+  margin-left: 0.5rem;
+  flex: 1 1 0%;
+  min-width: 0;
+}
+
+.greeting .title,
+.greeting .title input {
+  color: var(--dark);
+  font-size: 1.5rem;
+  font-weight: 700;
+}
+
+.create-todo input[type="text"] {
+  display: block;
+  width: 100%;
+  font-size: 1.125rem;
+  padding: 1rem 1.5rem;
+  color: var(--dark);
+  background-color: #FFF;
+  border-radius: 0.5rem;
+  box-shadow: var(--shadow);
+  margin-bottom: 1.5rem;
+}
+
+.create-todo .options {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1rem;
+  margin-bottom: 1.5rem;
+}
+
+.create-todo .options label {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 1.5rem;
+  background-color: #FFF;
+  border-radius: 0.5rem;
+  box-shadow: var(--shadow);
+  cursor: pointer;
+}
+
+input[type="radio"],
+input[type="checkbox"] {
+  display: none;
+}
+
+.bubble {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  border: 2px solid var(--business);
+  box-shadow: var(--business-glow);
+}
+
+.bubble.personal {
+  border-color: var(--personal);
+  box-shadow: var(--personal-glow);
+}
+
+.bubble::after {
+  content: "";
+  display: block;
+  opacity: 0;
+  width: 0px;
+  height: 0px;
+  background-color: var(--business);
+  box-shadow: var(--business-glow);
+  border-radius: 50%;
+  transition: 0.2s ease-in-out;
+}
+
+.bubble.personal::after {
+  background-color: var(--personal);
+  box-shadow: var(--personal-glow);
+}
+
+input:checked~.bubble::after {
+  width: 10px;
+  height: 10px;
+  opacity: 1;
+}
+
+.create-todo .options label div {
+  color: var(--dark);
+  font-size: 1.125rem;
+  margin-top: 1rem;
+}
+
+.create-todo input[type="submit"] {
+  display: block;
+  width: 100%;
+  font-size: 1.125rem;
+  padding: 1rem 1.5rem;
+  color: #FFF;
+  background-color: var(--primary);
+  border-radius: 0.5rem;
+  box-shadow: var(--personal-glow);
+  cursor: pointer;
+  transition: 0.2s ease-in-out;
+}
+
+.create-todo input[type="submit"]:hover {
+  opacity: 0.75;
+}
+
+.todo-list .list {
+  margin: 1rem 0;
+}
+
+.todo-list .todo-item {
+  display: flex;
+  align-items: center;
+  background-color: #FFF;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  box-shadow: var(--shadow);
+  margin-bottom: 1rem;
+}
+
+.todo-item label {
+  display: block;
+  margin-right: 1rem;
+  cursor: pointer;
+}
+
+.todo-item .todo-content {
+  flex: 1 1 0%;
+}
+
+.todo-item .todo-content input {
+  color: var(--dark);
+  font-size: 1.125rem;
+}
+
+.todo-item .actions {
+  display: flex;
+  align-items: center;
+}
+
+.todo-item .actions button {
+  display: block;
+  padding: 0.5rem;
+  border-radius: 0.25rem;
+  color: #FFF;
+  cursor: pointer;
+  transition: 0.2s ease-in-out;
+}
+
+.todo-item .actions button:hover {
+  opacity: 0.75;
+}
+
+.todo-item .actions .edit {
+  margin-right: 0.5rem;
+  background-color: var(--primary);
+}
+
+.todo-item .actions .delete {
+  background-color: var(--danger);
+}
+
+.todo-item.done .todo-content input {
+  text-decoration: line-through;
+  color: var(--grey);
+}
+</style>
